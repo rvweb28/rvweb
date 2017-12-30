394f3500068069933bb07b3aa920d9b7b7e783fa
Moritz Schramm <moritz.schramm1@gmail.com>
2017-12-30 11:39:24 +0100

    use netlify's form action to send email

---
+++ assets/scripts/contact.js
@@ -0,0 +1,35 @@
+function send() {
+
+  removeErrors();
+  if(check()) {
+
+    $(".loader").removeClass('hidden');
+    $(".loader").fadeIn();
+
+    var $form = $(this);
+    $.post($form.attr("action"), $form.serialize()).then(function() {
+
+      $("#email").val("");
+      $("#message").val("");
+      $("#alert3").removeClass('hidden');
+      $(".loader").fadeOut();
+    });
+  }
+}
+function check() {
+
+  var email = $("#email").val().trim();
+  var msg = $("#message").val().trim();
+
+  if(email == '' || msg == '') {
+
+    $("#alert1").removeClass('hidden');
+    return false;
+  }
+  return true;
+}
+function removeErrors() {
+  $("#alert1").addClass('hidden');
+  $("#alert2").addClass('hidden');
+  $("#alert3").addClass('hidden');
+}
