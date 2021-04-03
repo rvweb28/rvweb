59a711abaad23b2a3a4ed5f7d144848761e11eb3
Moritz Schramm <ms@mosch.io>
2021-04-03 21:11:17 +0200

    add new images

--- assets/scripts/gallery.js
+++ assets/scripts/gallery.js
@@ -74,12 +74,12 @@ $('#next-btn').on('click', function() {
     var imgSrc = $('div[data-img-no="'+currentImg+'"]').attr('data-gallery');
     var caption = $('div[data-img-no="'+currentImg+'"]').next().html();
 
-    modalImg.animate({left: '-100%'}, 150, function() {
+    modalImg.animate({left: '-100%'}, 200, function() {
 
       modalImg.css('left', '100%');
       modalImg.attr('src', imgSrc);
       captionText.html(caption);
-      modalImg.animate({left: '0'}, 100);
+      modalImg.animate({left: '0'}, 150);
     });
   }
 });
@@ -91,12 +91,12 @@ $('#previous-btn').on('click', function() {
     var imgSrc = $('div[data-img-no="'+currentImg+'"]').attr('data-gallery');
     var caption = $('div[data-img-no="'+currentImg+'"]').next().html();
 
-    modalImg.animate({left: '100%'}, 150, function() {
+    modalImg.animate({left: '100%'}, 200, function() {
 
       modalImg.css('left', '-100%');
       modalImg.attr('src', imgSrc);
       captionText.html(caption);
-      modalImg.animate({left: '0'}, 100);
+      modalImg.animate({left: '0'}, 150);
     });
   }
 });
